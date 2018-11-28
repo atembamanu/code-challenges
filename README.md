b7e2129c8184c1f06732127010b8c0d1a18c0d82
Steven Meads <sbmeads@gmail.com>
2018-11-27 22:01:44 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# code-challenges
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,46 @@
+let count = 0;
+
+function KaprekarsConstant(num) {
+  if (num === 6174) {
+    return count;
+  }
+  count++;
+  let ascNum = sortDecOrAsc(num, "asc");
+  let decNum = sortDecOrAsc(num);
+  return KaprekarsConstant(decNum - ascNum);
+}
+
+function addZero(length, numArray) {
+  switch (length) {
+    case 3:
+      numArray.push(0);
+      break;
+    case 2:
+      numArray.push(0, 0);
+      break;
+    case 1:
+      numArray.push(0, 0, 0);
+      break;
+    default:
+      return;
+  }
+}
+
+function sortDecOrAsc(num, order = "dec") {
+  const numArray = num
+    .toString()
+    .split("")
+    .map(i => Number(i));
+
+  if (numArray.length < 4) {
+    addZero(numArray.length, numArray);
+  }
+
+  if (order === "asc") {
+    return Number(numArray.sort((a, b) => a - b).join(""));
+  } else {
+    return Number(numArray.sort((a, b) => b - a).join(""));
+  }
+}
+
+KaprekarsConstant(2111);
