2e16c9422159e65c01337b8c7d700cd8c91f8dda
Steven Meads <sbmeads@gmail.com>
2018-11-28 21:43:21 -0700

    project-euler
    multiple of three and five solution

--- src/index.js
+++ src/index.js
@@ -44,3 +44,15 @@ function sortDecOrAsc(num, order = "dec") {
 }
 
 KaprekarsConstant(2111);
+
+function multiplesOfThreeAndFive() {
+  let sum = 0;
+  for (let i = 0; i <= 1000; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+multiplesOfThreeAndFive();
